d094e6ffecb8fbfb06200c282ff292693e78a427
Amanda Daly and Ned Faulhaber <techdaly@gmail.com>
2016-05-26 08:28:07 -0700

    Create files

---
+++ js/map-interface.js
@@ -0,0 +1,12 @@
+var map;
+$(document).ready(function(){
+  alert('hi');
+  initMap();
+});
+
+var initMap = function() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.5231, lng: -122.6765},
+    zoom: 10
+  });
+}
