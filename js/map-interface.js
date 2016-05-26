c5b4c993e278fc800031c84ed150375c7d5fdb87
Amanda Daly and Ned Faulhaber <nedfaulhaber@gmail.com>
2016-05-26 15:06:34 -0700

    add CSS, attempt to call API

--- js/map-interface.js
+++ js/map-interface.js
@@ -9,4 +9,12 @@ var initMap = function() {
 
 $(document).ready(function(){
   initMap();
+  $("form#search").submit(function(event){
+    event.preventDefault();
+    var userSearch = $("input#search-box").val();
+    $.get("https://maps.googleapis.com/maps/api/js?key=AIzaSyAEc12j5uuNs9ioaNkCITCBeVCvE0Qcg-4&q=Folsom,CA").then(function(response){
+      console.log(response);
+    });
+
+  });
 });
